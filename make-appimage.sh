87c1ab8894e2361fdc8d0d308954a16a2563d8ac
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-12-11 11:24:29 +0300

    Add metainfo file to basic config
    I think it's important now for appimage.github.io and other app lists

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=PATH_OR_URL_TO_ICON
 export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export METAINFO=PATH_OR_URL_TO_METAINFO
 
 # Deploy dependencies
 quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
